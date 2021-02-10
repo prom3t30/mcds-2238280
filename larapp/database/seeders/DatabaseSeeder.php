52873a7e16ef347027a1e32e0944f3ee616a2c0d
prom3t30 <andrus201232@gmail.com>
2021-02-09 20:53:39 -0500

    GAmeSeeder & UserFactory

--- larapp/database/seeders/DatabaseSeeder.php
+++ larapp/database/seeders/DatabaseSeeder.php
@@ -15,12 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
 
         $this->call([
         	UserSeeder::class,
         	CategorySeeder::class,
-        	//GameSeeder::class,
+        	GameSeeder::class,
     	]);
+
+        \App\Models\User::factory(500)->create();
     }
 }
